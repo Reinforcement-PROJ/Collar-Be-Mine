6eec3b0abe6a980dfd9ef717f849b3ddfb8f8d48
Linden Young <imlindenyoung@gmail.com>
2022-11-01 14:08:23 -0600

    update middleware index to include googleAuth

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,5 +2,6 @@ import authUser from './authUser.js';
 import jwtCreator from './jwtCreator.js';
 import dogList from './dogList.js';
 import newDog from './newDog.js';
+import googleAuth from './googleAuth.js';
 
-export { authUser, jwtCreator, dogList, newDog };
+export { authUser, jwtCreator, dogList, newDog, googleAuth };
