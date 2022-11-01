2575c3ded4288d7800d992a5339082fdcb8503bb
ryanpotter <rpotter0811@gmail.com>
2022-11-01 15:48:14 -0600

    add home page component

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Components/login/Login';
+import Home from './Components/login/Home';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} /> {/* after successful login or new user form submission */}
     </Routes>
   );
 };
