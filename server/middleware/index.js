8f0ff980b112fcd1a12cddf41664c19634f4ddcb
James Chan <108029209+j-chany@users.noreply.github.com>
2022-11-02 11:32:09 -0700

    added backend route for match

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -3,5 +3,5 @@ import jwtCreator from './jwtCreator.js';
 import dogList from './dogList.js';
 import newDog from './newDog.js';
 import googleAuth from './googleAuth.js';
-
-export { authUser, jwtCreator, dogList, newDog, googleAuth };
+import checkForLove from './checkForLove.js';
+export { authUser, jwtCreator, dogList, newDog, googleAuth, checkForLove };
